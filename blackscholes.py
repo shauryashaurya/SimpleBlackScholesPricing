d9d980af11a2688116174a56b7e9e4ccad65000d
Shaurya Agarwal <shauryashaurya@gmail.com>
2017-04-17 20:13:17 +0530

    Init
    Simple implementation of the Black Scholes option pricing model in
    Python, takes CSV input and generates a CSV output.

---
+++ blackscholes.py
@@ -0,0 +1,18 @@
+# Black Scholes option pricing
+import math
+import decimal
+
+#input definitions:
+#Current Stock Price
+u_price = 0.0
+#Strike price
+strike = 0.0
+#Days to expiration
+time = 0.0
+#Current interest rate
+rate = 0.0
+#The implied volitility (the Sigma)
+sigma = 0.0
+#Dividend rate
+dividend_rate = 0.0
+
